bb6695154eda74572b30a882f06aaf2df35c852a
sarazaghlool <es.sara.ahmed2023@alexu.edu.eg>
2025-10-23 14:48:24 +0300

    connected the frontend to the backend part

--- src/AddStudentPanel.java
+++ src/AddStudentPanel.java
@@ -6,8 +6,12 @@ public class AddStudentPanel extends JPanel {
     private JTextField nameField, ageField, idField, departmentField, GPAField;
     private JButton addButton, clearButton;
     private JComboBox<String> genderBox;
+    //backend variables
+    private StudentManager studentManager;
 
-    public AddStudentPanel(){
+    public AddStudentPanel(StudentManager studentManager){
+        this.studentManager = studentManager;       //connecting to backend (like a constructor)
+
         // Use GridLayout: 7 rows (6 fields + buttons), 2 columns
         setLayout(new GridLayout(7, 2, 10, 10));
         setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20)); // padding around edges
@@ -41,17 +45,41 @@ public class AddStudentPanel extends JPanel {
         add(departmentLabel); add(departmentField);
         add(GPALabel); add(GPAField);
         add(addButton); add(clearButton);
+
+        //buttons functionality
+        addButton.addActionListener(e -> addStudent());     //e is an object of action listener class.
+        clearButton.addActionListener(e -> clearFields());
     }
 
+    private void addStudent(){
+        //reading input from user.
+        String name = nameField.getText();
+        String id = idField.getText();
+        String department = departmentField.getText();
+        String gender = (String) genderBox.getSelectedItem();
+        int age = Integer.parseInt(ageField.getText());
+        float gpa = Float.parseFloat(GPAField.getText());
 
-        public static void main(String[] args) {
-            JFrame frame = new JFrame("Add Student");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //making a new student object
+        Student s = new Student(id, name,age, gender, department, gpa);
+        //now we need to tell the backend to add to the array and file
+        boolean success = studentManager.addStudent(s);
 
-            frame.add(new AddStudentPanel());
-
-            frame.setSize(600, 400);         // make it bigger
-            frame.setLocationRelativeTo(null); // center it on screen
-            frame.setVisible(true);
+        if(success){
+            JOptionPane.showMessageDialog(this, "Student added successfully!");     //this to display the message in this panel
         }
+        else{
+            JOptionPane.showMessageDialog(this, "Student with this ID already exists!");
+        }
+    }
+
+    //for the clear button
+    private void clearFields() {
+        nameField.setText("");
+        ageField.setText("");
+        idField.setText("");
+        departmentField.setText("");
+        GPAField.setText("");
+        genderBox.setSelectedIndex(0); // reset combo box to "Select"
+    }
 }
