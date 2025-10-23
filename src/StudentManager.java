293ecd0b592211ef66a81d7aa42cea90a21c9f5f
zeinaelbanna <zeinaelbanna5@gmail.com>
2025-10-23 11:37:20 +0300

    create method delete student

--- src/StudentManager.java
+++ src/StudentManager.java
@@ -1,18 +1,34 @@
 import java.util.ArrayList;
+import java.util.logging.FileHandler;
 
-public class StudentManager {
+public class StudentManager extends FileHandler {
     private ArrayList<Student> students;
     public StudentManager() {
         students = new ArrayList<>();
+        students=loadFromFile;
     }
     public boolean addStudent(Student s){
-        for(int i = 0; i < students.size(); i++){
-          Student exist =students.get(i);
-          if(exist.getId().equals(s.getId())){
-              return false;
-          }
-          students.add(s);
-          return true;
+        for(int i = 0; i < students.size(); i++) {
+            Student exist = students.get(i);
+            if (exist.getId().equals(s.getId())) {
+                return false;
+            }
         }
+          students.add(s);
+        saveToFile(students);
+          return true;
+
     }
+    public boolean deleteStudent(Student s){
+        for(int i = 0; i < students.size(); i++) {
+            Student exist = students.get(i);
+            if (exist.getId().equals(s.getId())) {
+                return false;
+            }
+        }
+        students.remove(s);
+        saveToFile(students);
+        return true;
+    }
+    
 }
