d6fad777c8b561063e55184f3958dcf0a49c6725
zeinaelbanna <zeinaelbanna5@gmail.com>
2025-10-23 11:23:59 +0300

    added constructor and add student methods

--- src/StudentManager.java
+++ src/StudentManager.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+
 public class StudentManager {
-    
+    private ArrayList<Student> students;
+    public StudentManager() {
+        students = new ArrayList<>();
+    }
+    public boolean addStudent(Student s){
+        for(int i = 0; i < students.size(); i++){
+          Student exist =students.get(i);
+          if(exist.getId().equals(s.getId())){
+              return false;
+          }
+          students.add(s);
+          return true;
+        }
+    }
 }
